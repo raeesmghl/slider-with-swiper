a5e5c0304741c40b6db60a815e1f8f1224ea8c3e
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-03-28 21:55:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    // loop:true,
+
+      navigation: {
+    nextEl: ".n-btn",
+    prevEl: ".p-btn",
+
+
+  },
+
+  });
+
+
+
+
+
+
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+
+
+//   },
